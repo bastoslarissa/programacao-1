5201f55f68cccc6e3deb06c9184a301c909ad852
Larissa de Souza Bastos <larissa.bst@hotmail.com>
2025-09-09 16:09:15 -0300

    tp3 [main com problemas]

--- tp3/tp3.c
+++ tp3/tp3.c
@@ -9,10 +9,52 @@
 #include <stdlib.h>
 #include "racional.h"
 
+
+struct racional le_vetor (int tam, struct racional *v) {
+
+  // preenche o vetor com n racionais lidos da entrada
+  for (int i = 0; i < tam; i++) {
+    scanf("%ld", &v[i].num);
+    scanf("%ld", &v[i].den);
+  }
+
+  return *v;
+}
+
+void imprime_vetor (int tam, struct racional *v) {
+
+  // imprime o vetor lido
+  printf("VETOR = ");
+  for (int i = 0; i < tam; i++) {
+    imprime_r(&v[i]);
+    printf(" ");
+  }
+
+}
+
+struct racional remove_invalidos (int tam, struct racional *v, int v_tam) {
+
+  // elimina os racionais inválidos do vetor
+  for (int i = 0; i < tam; i++) {
+    if (!(valido_r(&v[i]))) {
+      destroi_r(&v[i]);
+      v_tam = tam--;
+    }
+  }
+
+  return *v;
+}
+
+void ordena_racionais (struct racional *v, int tam) {
+
+  
+}
+
+
 /* programa principal */
 int main () {
 
-  int n;
+  int n, v_tam = 0;
 
   // lê um valor n tal que 0 < n < 100
   do 
@@ -20,33 +62,25 @@ int main () {
   while (n < 0 && n > 100);
 
   // aloca um vetor de tamanho n
-  struct racional *vetor = malloc(n * sizeof(struct racional)); 
+  struct racional *v = malloc(n * sizeof(struct racional)); 
 
   // preenche o vetor com n racionais lidos da entrada
-  for (int i = 0; i < n; i++) {
-    scanf("%ld", &vetor[i].num);
-    scanf("%ld", &vetor[i].den);
-  }
+  le_vetor(n, v);
 
   // imprime o vetor lido
-  printf("VETOR = ");
-  for (int i = 0; i < n; i++) {
-    imprime_r(&vetor[i]);
-    printf(" ");
-  }
+  imprime_vetor(n, v);
 
   // elimina os racionais inválidos do vetor
-  for (int i = 0; i < n; i++) {
-    if (!valido_r(&vetor[i]))
-      destroi_r(&vetor[i]);
-  }
+  remove_invalidos(n, v, v_tam);
 
   // imprime o vetor sem inválidos
-  printf("\nVETOR = ");
-  for (int i = 0; i < n; i++) {
-    imprime_r(&vetor[i]);
-    printf(" ");
-  }
+  imprime_vetor(v_tam, v);
+
+  // ordena o vetor em ordem crescente
+
+
+
+
 
 return (0) ;
 }
