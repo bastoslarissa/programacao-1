e44941351901bbd585c3b83639dc050567f159e1
Larissa de Souza Bastos <larissa.bst@hotmail.com>
2025-09-18 16:57:09 -0300

    tp3 [concertando função remove_invalidos]

--- tp3/tp3.c
+++ tp3/tp3.c
@@ -10,43 +10,52 @@
 #include "racional.h"
 
 
-struct racional le_vetor (int tam, struct racional *v) {
+void le_vetor (int tam, struct racional **v) {
+
+  long int numerador, denominador;
 
   // preenche o vetor com n racionais lidos da entrada
   for (int i = 0; i < tam; i++) {
-    scanf("%ld", &v[i].num);
-    scanf("%ld", &v[i].den);
-  }
+    scanf("%ld", numerador);
+    scanf("%ld", denominador);
 
-  return *v;
+    cria_r(numerador, denominador);
+
+  }
 }
 
-void imprime_vetor (int tam, struct racional *v) {
+void imprime_vetor (int tam, struct racional **v) {
 
   // imprime o vetor lido
   printf("VETOR = ");
   for (int i = 0; i < tam; i++) {
-    imprime_r(&v[i]);
+    imprime_r(v[i]);
     printf(" ");
   }
 
 }
 
-struct racional remove_invalidos (int tam, struct racional *v, int v_tam) {
+struct racional remove_invalidos (int tam, struct racional **v, int v_tam) {
 
   struct racional *aux; 
 
+  // percorre o vetor do início ao fim
   for (int i = 0; i < tam; i++) {
 
-    if (!valido_r(&v[i])) {
+    if (!valido_r(v[i])) {
+      
+      for (int j = tam; j <= tam; tam--) {
+        
+        if (!valido_r(v[j])) 
+          tam--;
 
-      for (int j = tam; j <= tam; j--) {
+        else if (valido(v[j])) {
+          v[i] = v[j];
+          tam--;
+          break;
+        }
 
-        if (valido_r(&v[j]))
-        *aux = v[i];
-        v[i] = v[j]; 
-        v[j] = *aux;
-      }
+      }      
     }
   }
 }
@@ -65,7 +74,7 @@ int main () {
   while (n < 0 && n > 100);
 
   // aloca um vetor de tamanho n
-  struct racional *v = malloc(n * sizeof(struct racional)); 
+  struct racional **v = malloc(n * sizeof(struct racional)); 
 
   // preenche o vetor com n racionais lidos da entrada
   le_vetor(n, v);
