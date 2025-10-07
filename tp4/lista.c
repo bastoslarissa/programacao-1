68892b97714594fa338811c5b2b248b741179d3d
Larissa de Souza Bastos <larissa.bst@hotmail.com>
2025-10-07 16:00:57 -0300

    tp4 [arquivo atualizado]

--- tp4/lista.c
+++ tp4/lista.c
@@ -1,56 +1,55 @@
-// TAD lista de números inteiros
-// Carlos Maziero - DINF/UFPR, Out 2024
-//
-// Implementação do TAD - a completar
-//
-// Implementação com lista encadeada dupla não-circular
-
-#include <stdio.h>
-#include <stdlib.h>
 #include "lista.h"
+/* coloque demais includes aqui */
 
-// estrutura de um item da lista
-struct item_t {
-  int valor;
-  struct item_t *ant;
-  struct item_t *prox;
-} ;
+/* ------------------- Nao altere estas structs ------------------------- */
+struct nodo {
+    int chave;
+    struct nodo *prox;
+};
 
-// estrutura de uma lista
-struct lista_t {
-  struct item_t *primeiro;
-  struct item_t *ultimo;
-  int num;
-} ;
+struct lista {
+    struct nodo *ini;
+    struct nodo *ptr; /* ponteiro para algum nodo da lista (iterador) */
+    int tamanho;
 
-// Cria uma lista vazia.
-// Retorno: ponteiro p/ a lista ou NULL em erro.
-struct lista_t *lista_cria () {
- 
-  struct lista_t *lista = malloc(sizeof(struct lista_t));
+};
+/* ---------------------------------------------------------------------- */
 
-  /* verifica se a lista é nula (inexistente) */
-  if (lista == NULL)
-    return NULL;
-
-  /* atribui nulo para as posições e zero para a quantidade de elementos da lista, porque é uma lista vazia */
-  lista->primeiro = NULL;
-  lista->ultimo = NULL;
-  lista->num = 0;
-  
-  return lista;
-
+struct lista *lista_cria (){
 }
 
-// Remove todos os itens da lista e libera a memória.
-// Retorno: NULL.
-struct lista_t *lista_destroi (struct lista_t *lst) {
-  
-  struct item_t *end;
-
-  for (int i = 0; i < lst->num; i++)
-    *end = lst->
-
-
+void lista_destroi (struct lista **lista){
 }
 
+int lista_insere_inicio (struct lista *lista, int chave){
+}
+
+int lista_insere_fim (struct lista *lista, int chave){
+}
+
+int lista_insere_ordenado (struct lista *lista, int chave){
+}
+
+int lista_remove_inicio (struct lista *lista, int *chave){
+}
+
+int lista_remove_fim (struct lista *lista, int *chave){
+}
+
+int lista_remove_ordenado (struct lista *lista, int chave){
+}
+
+int lista_vazia (struct lista *lista){
+}
+
+int lista_tamanho (struct lista *lista){
+}
+
+int lista_pertence (struct lista *lista, int chave){
+}
+
+void lista_inicia_iterador (struct lista *lista){
+}
+
+int lista_incrementa_iterador (struct lista *lista, int *chave){
+}
