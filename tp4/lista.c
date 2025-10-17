0ffb5fdfed8d875f5604502228fc60bd380b2492
Rafael Santos <rafa1405rafael@gmail.com>
2025-10-17 19:09:19 -0300

    tp4 [continuando o .c]

--- tp4/lista.c
+++ tp4/lista.c
@@ -1,5 +1,5 @@
 #include "lista.h"
-#include <stdlib.h>>
+#include <stdio.h>
 #include <stdlib.h>
 
 /* ------------------- Nao altere estas structs ------------------------- */
@@ -18,22 +18,44 @@ struct lista {
 
 struct lista *lista_cria () {
 
-    struct lista *list = mallloc(sizeof(struct lista));
+    struct lista *list = malloc(sizeof(struct lista));
 
     if (!list)
         return NULL;
+
+    list -> ini = NULL;
+    list -> ptr = NULL;
+    list -> tamanho = 0;
     
     return list; 
 }
 
 void lista_destroi (struct lista **lista) {
 
+    struct nodo *aux;
+
+    for (int i = 0; i < (**lista).tamanho; i++) {
+        aux = (**lista).ini;
+        (**lista).ini = (**lista).ini -> prox;
+        free (aux);
+    }
+
 }
 
-int lista_insere_inicio (struct lista *lista, int chave){
+int lista_insere_inicio (struct lista *lista, int chave) {
+
+    if (!lista)
+        return NULL;
+
+    lista -> ini = chave;
+
+    if (lista -> ini == chave) 
+        return 1;
+    else 
+        return 0;
 }
 
-int lista_insere_fim (struct lista *lista, int chave){
+/* int lista_insere_fim (struct lista *lista, int chave){
 }
 
 int lista_insere_ordenado (struct lista *lista, int chave){
@@ -61,4 +83,4 @@ void lista_inicia_iterador (struct lista *lista){
 }
 
 int lista_incrementa_iterador (struct lista *lista, int *chave){
-}
+} */
